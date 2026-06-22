4005a76a47bccb8a8c8d13ea406eb421b6b46b61
reza9978gdrz <rezagholialigudarzi@gmail.com>
2026-06-22 18:56:24 +0430

    last changes(models.py & admin.py)

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,12 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length = 255)
     content = models.TextField()
+    count_view = models.IntegerField(default= 0)
+    status = models.BooleanField(default= False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add = True)
+    updated_date = models.DateTimeField(auto_now = True)
+
+
+    def __str__(self):
+        return "{} , {}".format(self.title,self.id)
