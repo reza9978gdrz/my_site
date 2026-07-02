95f1c639c09da88ad49be6d5650b20c0a2572e95
reza9978gdrz <rezagholialigudarzi@gmail.com>
2026-07-02 19:08:42 +0430

    تکمیل کردن single bloge برای هر پست

--- blog/views.py
+++ blog/views.py
@@ -5,16 +5,18 @@ import datetime as dt
 from django.db.models import F
 def blog_view(request):
      posts = Post.objects.filter(published_date__lte = dt.date.today())
-     
-     Post.objects.all().update(count_view=F('count_view')+1)
+     #Post.objects.all().update(count_view=F('count_view')+1)
      context = {'posts':posts}
-     return render(request,'website/blog-home.html',context)
+     return render(request,'blog/blog-home.html',context)
 
-def blog_single(request):
-    return render(request,'website/blog-single.html')
-
-def test(request,pid):
+def blog_single(request,pid):
     post = Post.objects.get(id = pid)
+    #post = Post.objects.update(count_view=F('count_view')+1)
+    post.count_view = post.count_view + 1
+    post.save()
     context = {'post':post}
-    return render(request,"website/test.html",context)
+    return render(request,'blog/blog-single.html',context)
+
+def test(request):
+    return render(request,"website/test.html")
 # Create your views here.
